83bb94477b4d499bf667d299a61aeb28e58daa63
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2018-01-02 19:06:14 +0100

    Better project structure

---
+++ src/main/groovy/pl/vanta/adventofcode/day1/Solution.groovy
@@ -0,0 +1,10 @@
+package pl.vanta.adventofcode.day1
+
+
+class Solution {
+    static void main(String... args) {
+        def input = getClass().getResource("/day1.txt").text
+        
+        
+    }
+}
