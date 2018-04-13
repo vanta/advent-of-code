ece7e99ef35f4ceceec70d7c7ca0aa130464cf41
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2018-04-13 22:32:05 +0200

    Solved day3 - part1

--- src/main/groovy/pl/vanta/adventofcode/year2017/day3/Solver3.groovy
+++ src/main/groovy/pl/vanta/adventofcode/year2017/day3/Solver3.groovy
@@ -1,18 +1,29 @@
 package pl.vanta.adventofcode.year2017.day3
 
-import static java.lang.Math.*
-
+import static java.lang.Math.abs
+import static java.lang.Math.ceil
+import static java.lang.Math.floor
+import static java.lang.Math.pow
+import static java.lang.Math.sqrt
 
 class Solver3 {
     static int solve(String input) {
-        def d = sqrt(input.toInteger())
-        def ceil = ceil(d)
-        def circle = floor(ceil / 2d)
-        
-        
-        
-        circle
+        def i = input.toInteger()
+
+        i == 1 ? 0 : calculate(i)
     }
 
+    static int calculate(int i) {
+        def d = sqrt(i)
+        def ceila = ceil(d)
+        def circle = floor(ceila / 2d)
+        long circleMax = pow(2 * (circle + 1) - 1, 2).toInteger()
+        long circleMin = pow(2 * circle - 1, 2).toInteger() + 1
+        long circleSize = circleMax - circleMin + 1
+        long circleSide = circleSize / 4
+        long dev = abs((circleSide / 2) - abs(i % circleSide - 1))
+
+        circle + dev
+    }
 
 }
