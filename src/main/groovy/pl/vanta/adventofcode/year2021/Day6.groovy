78e1d2f2b55decc8402edd0f3019b3af020d9e10
krzysztof.wolny <k.wolny@gmail.com>
2021-12-06 21:45:30 +0100

    Day 6 - part 2

--- src/main/groovy/pl/vanta/adventofcode/year2021/Day6.groovy
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day6.groovy
@@ -2,64 +2,70 @@ package pl.vanta.adventofcode.year2021
 
 class Day6 {
     private static final int DAYS = 80
+    private static final int DAYS2 = 256
 
     static List<Integer> parse(String input) {
         input.split(',').collect { it as int }
     }
 
     static int solve(List<Integer> numbers) {
-        def fishes = numbers.collect { new Fish(days: it) }
+        def fishes = new Fishes(numbers)
         println "Initial: $fishes"
 
         for (int i = 0; i < DAYS; i++) {
-            fishes.addAll(processDay(fishes))
+            fishes.dayPasses()
 
             println "After $i days: $fishes"
         }
 
-        fishes.size()
+        fishes.count()
     }
 
-    private static List<Fish> processDay(List<Fish> fishes) {
-        def newFish = []
-        for (def fish : fishes) {
-            def isNewFish = fish.dayPasses()
+    static long solve2(List<Integer> numbers) {
+        def fishes = new Fishes(numbers)
+        println "Initial: $fishes"
 
-            if (isNewFish) {
-                newFish << new Fish()
+        for (int i = 0; i < DAYS2; i++) {
+            fishes.dayPasses()
+
+            println "After $i days: $fishes"
+        }
+
+        fishes.count()
+    }
+
+    static class Fishes {
+        long[] shoal = new long[9]
+
+        Fishes(List<Integer> fish) {
+            fish.every {
+                shoal[it] += 1
             }
         }
 
-        newFish
-    }
+        void dayPasses() {
+            long[] newShoal = new long[9]
 
-    static int solve2(List<Integer> numbers) {
-        0
-    }
-
-    static class Fish {
-        private int days
-
-        Fish(final int days) {
-            this.days = days
-        }
-
-        Fish() {
-            this(8)
-        }
-
-        boolean dayPasses() {
-            if(days-- == 0) {
-                days = 6
-                return true
-            }
+            newShoal[0] = shoal[1]
+            newShoal[1] = shoal[2]
+            newShoal[2] = shoal[3]
+            newShoal[3] = shoal[4]
+            newShoal[4] = shoal[5]
+            newShoal[5] = shoal[6]
+            newShoal[6] = shoal[7] + shoal[0]
+            newShoal[7] = shoal[8]
+            newShoal[8] = shoal[0]
             
-            return false
+            shoal = newShoal
+        }
+
+        long count() {
+            shoal.inject { a, b -> a + b }
         }
 
         @Override
         String toString() {
-            "$days"
+            "Fishes: $shoal"
         }
     }
 
--- src/test/groovy/pl/vanta/adventofcode/year2021/Day6SolverSpec.groovy
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day6SolverSpec.groovy
@@ -9,22 +9,22 @@ import static pl.vanta.adventofcode.year2021.Day6.solve2
 class Day6SolverSpec extends Specification {
     def 'should solve example data - part 1'() {
         expect:
-        solve(parse(Day6.getResource("/2021/example-day6.txt").text)) == 5934
+        solve(parse(Day6.getResource("/2021/example-day6.txt").text)) == 5_934
     }
 
     def 'should solve real data - part 1'() {
         expect:
-        solve(parse(Day6.getResource("/2021/day6.txt").text)) == 352872
+        solve(parse(Day6.getResource("/2021/day6.txt").text)) == 352_872
     }
 
     def 'should solve example data - part 2'() {
         expect:
-        solve2(parse(Day6.getResource("/2021/example-day6.txt").text)) == 12
+        solve2(parse(Day6.getResource("/2021/example-day6.txt").text)) == 26_984_457_539
     }
 
     def 'should solve real data - part 2'() {
         expect:
-        solve2(parse(Day6.getResource("/2021/day6.txt").text)) == 21038
+        solve2(parse(Day6.getResource("/2021/day6.txt").text)) == 1_604_361_182_149
     }
 
 }
