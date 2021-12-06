d53664eba75b0d221126fab92c0d5ad047044a46
krzysztof.wolny <k.wolny@gmail.com>
2021-12-06 19:28:49 +0100

    Day6 - parsing

---
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day6.groovy
@@ -0,0 +1,15 @@
+package pl.vanta.adventofcode.year2021
+
+class Day6 {
+    static List<Integer> parse(String input) {
+        input.split().collect { it as int }
+    }
+
+    static int solve(List<Integer> numbers) {
+        0
+    }
+
+    static int solve2(List<Integer> numbers) {
+        0
+    }
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day6SolverSpec.groovy
@@ -0,0 +1,30 @@
+package pl.vanta.adventofcode.year2021
+
+import spock.lang.Specification
+
+import static pl.vanta.adventofcode.year2021.Day6.parse
+import static pl.vanta.adventofcode.year2021.Day6.solve
+import static pl.vanta.adventofcode.year2021.Day6.solve2
+
+class Day6SolverSpec extends Specification {
+    def 'should solve example data - part 1'() {
+        expect:
+        solve(parse(Day6.getResource("/2021/example-day6.txt").text)) == 5
+    }
+
+    def 'should solve real data - part 1'() {
+        expect:
+        solve(parse(Day6.getResource("/2021/day6.txt").text)) == 7297
+    }
+
+    def 'should solve example data - part 2'() {
+        expect:
+        solve2(parse(Day6.getResource("/2021/example-day6.txt").text)) == 12
+    }
+
+    def 'should solve real data - part 2'() {
+        expect:
+        solve2(parse(Day6.getResource("/2021/day6.txt").text)) == 21038
+    }
+
+}
