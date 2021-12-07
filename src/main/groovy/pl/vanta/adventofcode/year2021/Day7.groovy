1f83377cd6f0e7bba742f59e413f4a9d1b5be2b5
krzysztof.wolny <k.wolny@gmail.com>
2021-12-08 00:42:04 +0100

    Day 7 - part 2

--- src/main/groovy/pl/vanta/adventofcode/year2021/Day7.groovy
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day7.groovy
@@ -22,27 +22,45 @@ class Day7 {
                 minFuel = fuel
             }
         }
-        
+
         minFuel
     }
 
-    static int checkPosition(int position, List<Integer> input) {
+    static int checkPosition(int position, List<Integer> positions) {
         int fuel = 0
-        for (int number : input) {
+        for (int number : positions) {
             fuel += abs(number - position)
         }
         fuel
     }
 
-    static long solve2(List<Integer> numbers) {
+    static long solve2(List<Integer> positions) {
+        println "Input size=${positions.size()}"
 
+        def max = positions.max()
+        def minFuel = MAX_VALUE
+
+        for (int position = 0; position <= max; position++) {
+            def fuel = checkPosition2(position, positions)
+
+            if (fuel < minFuel) {
+                minFuel = fuel
+            }
+        }
+
+        minFuel
     }
 
-    static int findPosition(List<Integer> numbers) {
-
+    static int checkPosition2(int position, List<Integer> positions) {
+        int fuel = 0
+        
+        for (int p : positions) {
+            fuel += sumFuel(abs(p - position))
+        }
+        fuel
     }
 
-    static int calculateFuel(List<Integer> numbers, int position) {
-        0
+    static int sumFuel(int n) {
+        n * (n + 1) / 2
     }
 }
--- src/test/groovy/pl/vanta/adventofcode/year2021/Day7SolverSpec.groovy
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day7SolverSpec.groovy
@@ -19,12 +19,12 @@ class Day7SolverSpec extends Specification {
 
     def 'should solve example data - part 2'() {
         expect:
-        solve2(parse(Day7.getResource("/2021/example-day7.txt").text)) == -1
+        solve2(parse(Day7.getResource("/2021/example-day7.txt").text)) == 168
     }
 
     def 'should solve real data - part 2'() {
         expect:
-        solve2(parse(Day7.getResource("/2021/day7.txt").text)) == -1
+        solve2(parse(Day7.getResource("/2021/day7.txt").text)) == 92439766
     }
 
 }
