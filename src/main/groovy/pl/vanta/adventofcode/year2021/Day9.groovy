db50d4b203904a3d5256c66878fcc966b1928321
krzysztof.wolny <k.wolny@gmail.com>
2021-12-09 08:36:32 +0100

    Day 9 - parsing

---
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day9.groovy
@@ -0,0 +1,25 @@
+package pl.vanta.adventofcode.year2021
+
+class Day9 {
+
+    static int[][] parse(String input) {
+        input.split('\n')
+                .collect { it.split('').collect { it as int }.toArray() }
+                .toArray()
+    }
+
+    static int solve(int[][] input) {
+        println("Input=$input")
+
+
+        0
+    }
+
+    static long solve2(int[][] input) {
+
+
+        0
+    }
+
+
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day9SolverSpec.groovy
@@ -0,0 +1,30 @@
+package pl.vanta.adventofcode.year2021
+
+import spock.lang.Specification
+
+import static pl.vanta.adventofcode.year2021.Day9.parse
+import static pl.vanta.adventofcode.year2021.Day9.solve
+import static pl.vanta.adventofcode.year2021.Day9.solve2
+
+class Day9SolverSpec extends Specification {
+    def 'should solve example data - part 1'() {
+        expect:
+        solve(parse(Day9.getResource("/2021/example-day9.txt").text)) == 15
+    }
+
+    def 'should solve real data - part 1'() {
+        expect:
+        solve(parse(Day9.getResource("/2021/day9.txt").text)) == -1
+    }
+
+    def 'should solve example data - part 2'() {
+        expect:
+        solve2(parse(Day9.getResource("/2021/example-day9.txt").text)) == 61_229
+    }
+
+    def 'should solve real data - part 2'() {
+        expect:
+        solve2(parse(Day9.getResource("/2021/day9.txt").text)) == 986_034
+    }
+
+}
