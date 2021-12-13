a298d9ad2b5dfd2cc632a84aa2a8e3373f2d37d2
krzysztof.wolny <k.wolny@gmail.com>
2021-12-13 22:58:39 +0100

    Day 13 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2021/Day13.groovy
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day13.groovy
@@ -11,14 +11,17 @@ class Day13 {
         def dots = input.split('\n')
                 .findAll { it.contains(',') }
                 .collect { it.split(',') }
-                .collect { new Tuple2<>(it[0] as int, it[1] as int) }
+                .collect { new Tuple2<>(it[0] as int, it[1] as int) } as Set
 
-        new Input(dots: dots, folds: folds)
+        new Input(paper: new Paper(dots), folds: folds)
     }
 
     static int solve(Input input) {
+        def fold = input.folds[0]
 
-        0
+        input.paper.fold(fold.getOrientation(), fold.getValue())
+
+        input.paper.countDots()
     }
 
     static long solve2(Input input) {
@@ -27,12 +30,49 @@ class Day13 {
     }
 
     static class Input {
-        List<Tuple2<Integer, Integer>> dots = []
+        Paper paper
         List<Fold> folds = []
     }
 
     static class Fold {
         char orientation
         int value
+    }
+
+    static class Paper {
+        private static final X = 'x' as char
+        private static final Y = 'y' as char
+        Set<Tuple2<Integer, Integer>> dots
+        int maxX
+        int maxY
+
+        Paper(Set<Tuple2<Integer, Integer>> dots) {
+            this.dots = dots
+
+            maxX = dots.collect { it.getFirst() }.max()
+            maxY = dots.collect { it.getSecond() }.max()
+        }
+
+        int countDots() {
+            dots.size()
+        }
+
+        void fold(char orientation, int value) {
+            def dotsToRemove
+            def dotsToAdd
+
+            if (X == orientation) {
+                dotsToRemove = dots.findAll { it.getFirst() > value }
+                dotsToAdd = dotsToRemove.collect { new Tuple2<>(maxX - it.getFirst(), it.getSecond()) }
+            } else {
+                dotsToRemove = dots.findAll { it.getSecond() > value }
+                dotsToAdd = dotsToRemove.collect { new Tuple2<>(it.getFirst(), maxY - it.getSecond()) }
+            }
+
+            dots.removeAll(dotsToRemove)
+            dots.addAll(dotsToAdd)
+
+            println("Dots=$dots")
+        }
     }
 }
--- src/test/groovy/pl/vanta/adventofcode/year2021/Day13SolverSpec.groovy
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day13SolverSpec.groovy
@@ -9,12 +9,12 @@ import static pl.vanta.adventofcode.year2021.Day13.solve2
 class Day13SolverSpec extends Specification {
     def 'should solve example data - part 1'() {
         expect:
-        solve(parse(Day13.getResource("/2021/example-day13.txt").text)) == -1
+        solve(parse(Day13.getResource("/2021/example-day13.txt").text)) == 17
     }
 
     def 'should solve real data - part 1'() {
         expect:
-        solve(parse(Day13.getResource("/2021/day13.txt").text)) == -1
+        solve(parse(Day13.getResource("/2021/day13.txt").text)) == 818
     }
 
     def 'should solve example data - part 2'() {
