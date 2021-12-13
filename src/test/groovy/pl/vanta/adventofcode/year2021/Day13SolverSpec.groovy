6a8fa48f5522f0177cf4c4b95537f776745382c5
krzysztof.wolny <k.wolny@gmail.com>
2021-12-13 21:40:42 +0100

    Day 13 - parsing

---
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day13SolverSpec.groovy
@@ -0,0 +1,29 @@
+package pl.vanta.adventofcode.year2021
+
+import spock.lang.Specification
+
+import static pl.vanta.adventofcode.year2021.Day13.parse
+import static pl.vanta.adventofcode.year2021.Day13.solve
+import static pl.vanta.adventofcode.year2021.Day13.solve2
+
+class Day13SolverSpec extends Specification {
+    def 'should solve example data - part 1'() {
+        expect:
+        solve(parse(Day13.getResource("/2021/example-day13.txt").text)) == -1
+    }
+
+    def 'should solve real data - part 1'() {
+        expect:
+        solve(parse(Day13.getResource("/2021/day13.txt").text)) == -1
+    }
+
+    def 'should solve example data - part 2'() {
+        expect:
+        solve2(parse(Day13.getResource("/2021/example-day13.txt").text)) == -1
+    }
+
+    def 'should solve real data - part 2'() {
+        expect:
+        solve2(parse(Day13.getResource("/2021/day13.txt").text)) == -1
+    }
+}
