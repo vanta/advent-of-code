2b4fcd050edfaa12e8cd16a3a5168fb27f523e30
krzysztof.wolny <k.wolny@gmail.com>
2021-12-16 10:44:23 +0100

    Day 15 - part 1

---
+++ src/main/groovy/pl/vanta/adventofcode/year2021/Day15.groovy
@@ -0,0 +1,99 @@
+package pl.vanta.adventofcode.year2021
+
+
+import static java.lang.Integer.MAX_VALUE
+
+class Day15 {
+    static int[][] parse(String input) {
+        input.split('\n')
+                .collect { it.split('').collect { it as int }.toArray() }
+                .toArray()
+    }
+
+    static long solve(int[][] input) {
+        List<Vertex> visited = []
+        Map<Tuple2, Vertex> mapping = buildMapping(input)
+        PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>(mapping.size(), { a, b -> a.distance <=> b.distance })
+//        PriorityQueue<Vertex> queue = new PriorityQueue<Vertex>(mapping.size(), new Comparator<Vertex>() {
+//            @Override
+//            int compare(final Vertex o1, final Vertex o2) {
+//                return o1.distance <=> o2.distance
+//            }
+//        })
+
+        queue.addAll(mapping.values())
+
+        def start = mapping.get(new Tuple2(0, 0))
+        def end = mapping.get(new Tuple2(input.length - 1, input.length - 1))
+
+        while (!queue.isEmpty()) {
+
+            def vertex = queue.remove()
+
+            visited << vertex
+
+            for (def d : getNeighbours(mapping, vertex)) {
+                if (queue.contains(d)) {
+                    def dist = vertex.distance + input[d.x][d.y]
+                    if (d.distance > dist) {
+                        d.distance = dist
+                        queue.remove(d)
+                        queue.add(d)
+                    }
+                }
+            }
+        }
+
+        end.distance
+    }
+
+    static List<Vertex> getNeighbours(Map<Tuple2, Vertex> mapping, Vertex vertex) {
+        [
+                mapping.get(new Tuple2(vertex.x - 1, vertex.y)),
+                mapping.get(new Tuple2(vertex.x + 1, vertex.y)),
+                mapping.get(new Tuple2(vertex.x, vertex.y - 1)),
+                mapping.get(new Tuple2(vertex.x, vertex.y + 1)),
+        ].findAll {
+            it != null
+        }
+    }
+
+    private static Vertex buildVertex(int i, int j) {
+        if (i == 0 && j == 0) {
+            new Vertex(x: i, y: j, distance: 0)
+        } else {
+            new Vertex(x: i, y: j)
+        }
+    }
+
+    static Map<Tuple2, Vertex> buildMapping(int[][] input) {
+        Map<Tuple2, Vertex> mapping = [:]
+
+        for (int i = 0; i < input.length; i++) {
+            for (int j = 0; j < input.length; j++) {
+                mapping.put(new Tuple2(i, j), buildVertex(i, j))
+            }
+        }
+
+        mapping
+    }
+
+    static long solve2(int[][] input) {
+
+    }
+
+    static class Vertex implements Comparable<Vertex> {
+        int x, y
+        int distance = MAX_VALUE
+
+        @Override
+        int compareTo(final Vertex v) {
+            distance <=> v.distance
+        }
+
+        @Override
+        String toString() {
+            "$x,$y ($distance)"
+        }
+    }
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2021/Day15SolverSpec.groovy
@@ -0,0 +1,29 @@
+package pl.vanta.adventofcode.year2021
+
+import spock.lang.Specification
+
+import static pl.vanta.adventofcode.year2021.Day15.parse
+import static pl.vanta.adventofcode.year2021.Day15.solve
+import static pl.vanta.adventofcode.year2021.Day15.solve2
+
+class Day15SolverSpec extends Specification {
+    def 'should solve example data - part 1'() {
+        expect:
+        solve(parse(Day15.getResource("/2021/example-day15.txt").text)) == 40
+    }
+
+    def 'should solve real data - part 1'() {
+        expect:
+        solve(parse(Day15.getResource("/2021/day15.txt").text)) == 583
+    }
+
+    def 'should solve example data - part 2'() {
+        expect:
+        solve2(parse(Day15.getResource("/2021/example-day15.txt").text)) == 315
+    }
+
+    def 'should solve real data - part 2'() {
+        expect:
+        solve2(parse(Day15.getResource("/2021/day15.txt").text)) == 1
+    }
+}
