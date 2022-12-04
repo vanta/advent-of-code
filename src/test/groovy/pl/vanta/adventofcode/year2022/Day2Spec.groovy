b51839be7a4a00153190fabd912cf8123a08f7d7
krzysztof.wolny <k.wolny@gmail.com>
2022-12-04 23:42:39 +0100

    2022 - day 2, part1 + refactoring

--- src/test/groovy/pl/vanta/adventofcode/year2022/Day2Spec.groovy
+++ src/test/groovy/pl/vanta/adventofcode/year2022/Day2Spec.groovy
@@ -3,29 +3,29 @@ package pl.vanta.adventofcode.year2022
 import pl.vanta.adventofcode.BaseSpec
 import pl.vanta.adventofcode.ParserSolver
 
-class Day1Spec extends BaseSpec {
+class Day2Spec extends BaseSpec {
     @Override
     ParserSolver getParserSolver() {
-        new Day1()
+        new Day2()
     }
 
     @Override
-    Integer getExampleAnswer1() {
-        24000
+    Object getExampleAnswer1() {
+        15
     }
 
     @Override
-    Integer getRealAnswer1() {
-        67016
+    Object getRealAnswer1() {
+        13221
     }
 
     @Override
-    Integer getExampleAnswer2() {
-        45000
+    Object getExampleAnswer2() {
+        return null
     }
 
     @Override
-    Integer getRealAnswer2() {
-        200116
+    Object getRealAnswer2() {
+        return null
     }
 }
