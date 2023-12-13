9cf4500a94f6d4e344b0c6bbb3ae471fa5039e39
krzysztof.wolny <k.wolny@gmail.com>
2023-12-13 01:36:24 +0100

    Day 11 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2023/Day11.java
+++ src/main/groovy/pl/vanta/adventofcode/year2023/Day11.java
@@ -2,9 +2,15 @@ package pl.vanta.adventofcode.year2023;
 
 import pl.vanta.adventofcode.ParserSolver;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.stream.Stream;
 
-public class Day11 implements ParserSolver<char[][], Integer> {
+import static java.lang.Math.abs;
+
+public class Day11 implements ParserSolver<List<String>, Integer> {
 
     @Override
     public int getDayNumber() {
@@ -12,20 +18,107 @@ public class Day11 implements ParserSolver<char[][], Integer> {
     }
 
     @Override
-    public char[][] parse(String lines) {
+    public List<String> parse(String lines) {
         return Stream.of(lines.split("\n"))
+                .toList();
+    }
+
+    @Override
+    public Integer solve(List<String> parsedInput) {
+        var expanded = expand(parsedInput);
+
+        var galaxies = findGalaxies(expanded);
+
+        galaxies.forEach(System.out::println);
+
+        var pairs = generatePairs(galaxies);
+
+        return pairs.stream()
+                .map(Pair::distance)
+                .reduce(0, Integer::sum);
+    }
+
+    private Set<Pair> generatePairs(List<Galaxy> galaxies) {
+        var result = new HashSet<Pair>();
+        for (int i = 0; i < galaxies.size(); i++) {
+            for (int j = i + 1; j < galaxies.size(); j++) {
+                result.add(new Pair(galaxies.get(i), galaxies.get(j)));
+            }
+        }
+        return result;
+    }
+
+    private List<Galaxy> findGalaxies(char[][] expanded) {
+        List<Galaxy> galaxies = new ArrayList<>();
+        for (int i = 0; i < expanded.length; i++) {
+            for (int j = 0; j < expanded[i].length; j++) {
+                if (expanded[i][j] == '#') {
+                    galaxies.add(new Galaxy(i, j));
+                }
+            }
+        }
+
+        return galaxies;
+    }
+
+    private char[][] expand(List<String> parsedInput) {
+        var result = new ArrayList<String>();
+        for (String s : parsedInput) {
+            result.add(s);
+            if (!s.contains("#")) {
+                result.add(s);
+            }
+        }
+
+        var result2 = new ArrayList<StringBuilder>();
+        int len = result.get(0).length();
+
+        for (int i = 0; i < len; i++) {
+            var shouldAdd = !hasGalaxy(result, i);
+
+            for (int j = 0; j < result.size(); j++) {
+                result2.add(new StringBuilder());
+            }
+
+            for (int j = 0; j < result.size(); j++) {
+                result2.get(j).append(result.get(j).charAt(i));
+
+                if (shouldAdd) {
+                    result2.get(j).append('.');
+                }
+            }
+
+        }
+
+        return result2.stream()
+                .map(StringBuilder::toString)
                 .map(String::toCharArray)
                 .toArray(char[][]::new);
     }
 
-    @Override
-    public Integer solve(char[][] parsedInput) {
+    private static List<StringBuilder> insertAt(List<StringBuilder> list, int i, char c) {
+        return list.stream()
+                .map(s -> s.insert(i, c))
+                .toList();
+    }
 
-        return 0;
+    private static boolean hasGalaxy(List<String> result, int i) {
+        return result.stream()
+                .map(s -> s.charAt(i))
+                .anyMatch(c -> c == '#');
     }
 
     @Override
-    public Integer solve2(char[][] parsedInput) {
+    public Integer solve2(List<String> parsedInput) {
         return 0;
+    }
+
+    private record Galaxy(int x, int y) {
+    }
+
+    private record Pair(Galaxy g1, Galaxy g2) {
+        int distance() {
+            return abs(g1.x() - g2.x()) + abs(g1.y() - g2.y());
+        }
     }
 }
