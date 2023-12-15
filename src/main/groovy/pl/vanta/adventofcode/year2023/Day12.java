318c219fbb1c0fb7736842d7a698bef5af2a27f2
krzysztof.wolny <k.wolny@gmail.com>
2023-12-15 23:05:04 +0100

    Day 12 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2023/Day12.java
+++ src/main/groovy/pl/vanta/adventofcode/year2023/Day12.java
@@ -4,7 +4,6 @@ import pl.vanta.adventofcode.ParserSolver;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
