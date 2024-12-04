ccf0a6b9af03d16f0a88ed9606bb768d477d9cf7
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-04 10:31:26 +0100

    2024 - Day 4 - part 1

---
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day4.java
@@ -0,0 +1,68 @@
+package pl.vanta.adventofcode.year2024;
+
+import java.util.Arrays;
+
+import pl.vanta.adventofcode.ParserSolver;
+
+public class Day4 implements ParserSolver<char[][], Integer> {
+
+    private static final String XMAS = "XMAS";
+
+    @Override
+    public int getDayNumber() {
+        return 4;
+    }
+
+    @Override
+    public char[][] parse(String lines) {
+        return Arrays.stream(lines.split("\n"))
+                .map(String::toCharArray)
+                .toArray(char[][]::new);
+    }
+
+    @Override
+    public Integer solve(char[][] parsedInput) {
+        int count = 0;
+        for (int i = 0; i < parsedInput.length; i++) {
+            for (int j = 0; j < parsedInput[i].length; j++) {
+                if (parsedInput[i][j] == 'X') {
+                    count += search(i, j, new Direction(1, 0), parsedInput);
+                    count += search(i, j, new Direction(0, 1), parsedInput);
+                    count += search(i, j, new Direction(1, 1), parsedInput);
+                    count += search(i, j, new Direction(1, -1), parsedInput);
+                    count += search(i, j, new Direction(-1, 1), parsedInput);
+                    count += search(i, j, new Direction(-1, -1), parsedInput);
+                    count += search(i, j, new Direction(-1, 0), parsedInput);
+                    count += search(i, j, new Direction(0, -1), parsedInput);
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private int search(int x, int y, Direction direction, char[][] parsedInput) {
+        var l1 = checkLetter(x, y, parsedInput, 'X');
+        var l2 = checkLetter(x + direction.x, y + direction.y, parsedInput, 'M');
+        var l3 = checkLetter(x + direction.x + direction.x, y + direction.y + direction.y, parsedInput, 'A');
+        var l4 = checkLetter(x + direction.x + direction.x + direction.x, y + direction.y + direction.y + direction.y, parsedInput, 'S');
+
+        return l1 && l2 && l3 && l4 ? 1 : 0;
+    }
+
+    private boolean checkLetter(int x, int y, char[][] parsedInput, char expected) {
+        if (x < 0 || y < 0 || x >= parsedInput.length || y >= parsedInput[x].length) {
+            return false;
+        }
+
+        return parsedInput[x][y] == expected;
+    }
+
+    @Override
+    public Integer solve2(char[][] parsedInput) {
+        return 0;
+    }
+
+    private record Direction(int x, int y) {
+    }
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day4Spec.groovy
@@ -0,0 +1,31 @@
+package pl.vanta.adventofcode.year2024
+
+import pl.vanta.adventofcode.BaseSpec
+import pl.vanta.adventofcode.ParserSolver
+
+class Day4Spec extends BaseSpec {
+    @Override
+    ParserSolver getParserSolver() {
+        new Day4()
+    }
+
+    @Override
+    Integer getExampleAnswer1() {
+        18
+    }
+
+    @Override
+    Integer getRealAnswer1() {
+        2500
+    }
+
+    @Override
+    Integer getExampleAnswer2() {
+        0
+    }
+
+    @Override
+    Integer getRealAnswer2() {
+        0
+    }
+}
