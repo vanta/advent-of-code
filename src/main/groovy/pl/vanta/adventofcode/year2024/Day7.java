75dbfa2e3bc0821732e39d3ad8dbdebe669742ec
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-07 11:21:26 +0100

    2024 - Day 7 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day7.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day7.java
@@ -2,14 +2,16 @@ package pl.vanta.adventofcode.year2024;
 
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 import pl.vanta.adventofcode.ParserSolver;
 
-import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
 import static java.util.Arrays.stream;
+import static java.util.Map.Entry;
+import static java.util.Map.entry;
+import static java.util.stream.Collectors.toMap;
 
-public class Day7 implements ParserSolver<Map<Integer, List<Integer>>, Integer> {
+public class Day7 implements ParserSolver<Map<Long, List<Integer>>, Long> {
 
     @Override
     public int getDayNumber() {
@@ -17,36 +19,37 @@ public class Day7 implements ParserSolver<Map<Integer, List<Integer>>, Integer> 
     }
 
     @Override
-    public Map<Integer, List<Integer>> parse(String lines) {
+    public Map<Long, List<Integer>> parse(String lines) {
         return stream(lines.split("\n"))
                 .map(l -> l.split(":"))
-                .collect(Collectors.toMap(
-                        a -> parseInt(a[0].trim()),
-                        a -> stream(a[1].trim().split(" "))
-                                .map(Integer::parseInt)
-                                .toList()
-                ));
+                .map(a -> entry(parseLong(a[0].trim()), numbersToList(a[1])))
+                .collect(toMap(Entry::getKey, Entry::getValue));
+    }
+
+    private static List<Integer> numbersToList(String s) {
+        return stream(s.trim().split(" "))
+                .map(Integer::parseInt)
+                .toList();
     }
 
     @Override
-    public Integer solve(Map<Integer, List<Integer>> parsedInput) {
+    public Long solve(Map<Long, List<Integer>> parsedInput) {
         return parsedInput.entrySet().stream()
                 .filter(e -> isValid(e.getKey(), e.getValue()))
-                .map(Map.Entry::getKey)
-                .reduce(0, Integer::sum);
+                .map(Entry::getKey)
+                .reduce(0L, Long::sum);
     }
 
-    private boolean isValid(int result, List<Integer> values) {
-
+    private boolean isValid(long result, List<Integer> values) {
 
 
         return false;
     }
 
     @Override
-    public Integer solve2(Map<Integer, List<Integer>> parsedInput) {
+    public Long solve2(Map<Long, List<Integer>> parsedInput) {
 
-        return 0;
+        return 0L;
     }
 
 }
