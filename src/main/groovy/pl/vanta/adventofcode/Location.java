4e8c297c0503780c5dd56e0cc92c0565ea07e703
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-11 00:24:44 +0100

    2024 - Day 10 - part 1

--- src/main/groovy/pl/vanta/adventofcode/Location.java
+++ src/main/groovy/pl/vanta/adventofcode/Location.java
@@ -1,4 +1,25 @@
 package pl.vanta.adventofcode;
 
+import java.util.Set;
+
 public record Location(int x, int y) {
+    public Location up() {
+        return new Location(x - 1, y);
+    }
+
+    public Location down() {
+        return new Location(x + 1, y);
+    }
+
+    public Location left() {
+        return new Location(x, y - 1);
+    }
+
+    public Location right() {
+        return new Location(x, y + 1);
+    }
+
+    public Set<Location> neighbours() {
+        return Set.of(up(), down(), left(), right());
+    }
 }
--- src/main/groovy/pl/vanta/adventofcode/year2024/Day10.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day10.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import pl.vanta.adventofcode.Location;
 import pl.vanta.adventofcode.ParserSolver;
 
-import static java.lang.Integer.parseInt;
-import static java.util.Collections.swap;
+import static pl.vanta.adventofcode.Utils.inBounds;
 
 public class Day10 implements ParserSolver<char[][], Integer> {
+    private static final char ZERO = '0';
+    private static final char NINE = '9';
 
     @Override
     public int getDayNumber() {
@@ -25,8 +27,36 @@ public class Day10 implements ParserSolver<char[][], Integer> {
 
     @Override
     public Integer solve(char[][] parsedInput) {
+        return findZeros(parsedInput).stream()
+                .mapToInt(zero -> countScore(parsedInput, zero, ZERO))
+                .sum();
+    }
 
-        return -1;
+    private int countScore(char[][] array, Location location, int current) {
+        if (current == NINE) {
+            return 1;
+        }
+
+        var i = current + 1;
+        return location.neighbours()
+                .stream()
+                .filter(l -> inBounds(l, array.length, array[0].length))
+                .filter(l -> array[l.x()][l.y()] == i)
+                .map(l -> countScore(array, l, i))
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private List<Location> findZeros(char[][] parsedInput) {
+        var zeros = new ArrayList<Location>();
+        for (int i = 0; i < parsedInput.length; i++) {
+            for (int j = 0; j < parsedInput[i].length; j++) {
+                if (parsedInput[i][j] == ZERO) {
+                    zeros.add(new Location(i, j));
+                }
+            }
+        }
+        return zeros;
     }
 
     @Override
