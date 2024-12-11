6c6d97a05914269a58f920fde560bad8b67c184b
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-11 16:35:14 +0100

    2024 - Day 11 - part 2

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day11.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day11.java
@@ -2,44 +2,40 @@ package pl.vanta.adventofcode.year2024;
 
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.function.Function;
 
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
-import org.apache.commons.lang3.concurrent.Memoizer;
 import pl.vanta.adventofcode.ParserSolver;
 
 import static java.lang.Long.parseLong;
 
-public class Day11 implements ParserSolver<String[], Integer> {
+public class Day11 implements ParserSolver<Long[], Integer> {
     @Override
     public int getDayNumber() {
         return 11;
     }
 
     @Override
-    public String[] parse(String lines) {
-        return lines.replace("\n", "").trim().split(" ");
+    public Long[] parse(String lines) {
+        return Arrays.stream(lines.replace("\n", "").trim().split(" "))
+                .map(Long::parseLong)
+                .toArray(Long[]::new);
     }
 
     @Override
-    public Integer solve(String[] parsedInput) {
+    public Integer solve(Long[] parsedInput) {
         return blink(parsedInput, 25);
     }
 
-    private int blink(String[] parsedInput, int blinks) {
+    private int blink(Long[] parsedInput, int blinks) {
         var stones = parsedInput;
 
-        var map = new HashMap<String, String[]>();
+        var map = new HashMap<Long, Long[]>();
 
         for (int i = 0; i < blinks; i++) {
             System.out.print("Blink " + i + ", ");
             stones = Arrays.stream(stones)
-//                    .parallel()
                     .map(s -> map.computeIfAbsent(s, this::change))
                     .flatMap(Arrays::stream)
-                    .toArray(String[]::new);
+                    .toArray(Long[]::new);
 
             System.out.println("stones: " + stones.length);
         }
@@ -47,21 +43,24 @@ public class Day11 implements ParserSolver<String[], Integer> {
         return stones.length;
     }
 
-    private String[] change(String s) {
-        if ("0".equals(s)) {
-            return new String[]{"1"};
-        } else if (s.length() % 2 == 0) {
-            var size2 = s.length() / 2;
-            var s1 = parseLong(s.substring(0, size2));
-            var s2 = parseLong(s.substring(size2));
-            return new String[]{String.valueOf(s1), String.valueOf(s2)};
+    private Long[] change(long s) {
+        if (s == 0) {
+            return new Long[]{1L};
         } else {
-            return new String[]{String.valueOf(parseLong(s) * 2024)};
+            var string = String.valueOf(s);
+            if (string.length() % 2 == 0) {
+                var size2 = string.length() / 2;
+                var s1 = parseLong(string.substring(0, size2));
+                var s2 = parseLong(string.substring(size2));
+                return new Long[]{s1, s2};
+            } else {
+                return new Long[]{s * 2024};
+            }
         }
     }
 
     @Override
-    public Integer solve2(String[] parsedInput) {
+    public Integer solve2(Long[] parsedInput) {
         return blink(parsedInput, 75);
     }
 
