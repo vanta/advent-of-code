f3d0e22c3291e74881f2f9e58870622f0f47b70b
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-11 10:21:37 +0100

    2024 - Day 11 - prepare

---
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day11.java
@@ -0,0 +1,27 @@
+package pl.vanta.adventofcode.year2024;
+
+import pl.vanta.adventofcode.ParserSolver;
+
+public class Day11 implements ParserSolver<String[], Integer> {
+    @Override
+    public int getDayNumber() {
+        return 11;
+    }
+
+    @Override
+    public String[] parse(String lines) {
+        return lines.replace("\n", "").trim().split(" ");
+    }
+
+    @Override
+    public Integer solve(String[] parsedInput) {
+        return -1;
+    }
+
+    @Override
+    public Integer solve2(String[] parsedInput) {
+        return -1;
+    }
+
+
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day11Spec.groovy
@@ -0,0 +1,44 @@
+package pl.vanta.adventofcode.year2024
+
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+
+class Day11Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        2024
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day11()
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [result: 55312],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        652
+    }
+
+    @Override
+    List<Map> getTestData2() {
+        [
+                [result: 3, suffix: '-part2-m1'],
+                [result: 13, suffix: '-part2-m2'],
+                [result: 81, suffix: '-part2-m3'],
+                [result: 227, suffix: '-part2-m4'],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        1432
+    }
+}
