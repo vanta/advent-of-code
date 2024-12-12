80e6e7de9ac3fe9528a09ffdda97fbebaa96971e
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-12 17:33:31 +0100

    2024 - Day 12 - prep

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day12.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day12.java
@@ -1,16 +1,10 @@
 package pl.vanta.adventofcode.year2024;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
-import org.apache.commons.lang3.tuple.Pair;
 import pl.vanta.adventofcode.ParserSolver;
 
-import static java.lang.Long.parseLong;
-
-public class Day12 implements ParserSolver<Long[], Long> {
-    private static final Map<Pair<Long, Integer>, Long> CACHE = new HashMap<>();
+public class Day12 implements ParserSolver<char[][], Integer> {
 
     @Override
     public int getDayNumber() {
@@ -18,57 +12,20 @@ public class Day12 implements ParserSolver<Long[], Long> {
     }
 
     @Override
-    public Long[] parse(String lines) {
-        return Arrays.stream(lines.replace("\n", "").trim().split(" "))
-                .map(Long::parseLong)
-                .toArray(Long[]::new);
+    public char[][] parse(String lines) {
+        return Arrays.stream(lines.split("\n"))
+                .map(String::toCharArray)
+                .toArray(char[][]::new);
     }
 
     @Override
-    public Long solve(Long[] parsedInput) {
-        return blink(parsedInput, 25);
+    public Integer solve(char[][] parsedInput) {
+        return -1;
     }
 
     @Override
-    public Long solve2(Long[] parsedInput) {
-        return blink(parsedInput, 75);
-    }
-
-    private long blink(Long[] stones, int blinks) {
-        return Arrays.stream(stones)
-                .map(stone -> count(stone, blinks))
-                .reduce(0L, Long::sum);
-    }
-
-    private long count(long n, int b) {
-        if (b == 0) {
-            return 1;
-        }
-
-        var key = Pair.of(n, b);
-
-        if (!CACHE.containsKey(key)) {
-            CACHE.put(key, getValue(n, b));
-        }
-
-        return CACHE.get(key);
-    }
-
-    private long getValue(long n, int b) {
-        if (n == 0) {
-            return count(1, b - 1);
-        } else {
-            var string = String.valueOf(n);
-
-            if (string.length() % 2 == 0) {
-                var size2 = string.length() / 2;
-                var s1 = parseLong(string.substring(0, size2));
-                var s2 = parseLong(string.substring(size2));
-                return count(s1, b - 1) + count(s2, b - 1);
-            } else {
-                return count(n * 2024, b - 1);
-            }
-        }
+    public Integer solve2(char[][] parsedInput) {
+        return -1;
     }
 
 }
