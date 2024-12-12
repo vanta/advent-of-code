03bb32523b8156b5caee19c430bfacca1ae2ae1c
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-12 17:49:43 +0100

    2024 - Day 12 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day12.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day12.java
@@ -1,8 +1,13 @@
 package pl.vanta.adventofcode.year2024;
 
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
+import pl.vanta.adventofcode.Location;
 import pl.vanta.adventofcode.ParserSolver;
+
+import static pl.vanta.adventofcode.Utils.inBounds;
 
 public class Day12 implements ParserSolver<char[][], Integer> {
 
@@ -20,12 +25,48 @@ public class Day12 implements ParserSolver<char[][], Integer> {
 
     @Override
     public Integer solve(char[][] parsedInput) {
-        return -1;
+        Set<Location> visited = new HashSet<>();
+        Set<Region> regions = new HashSet<>();
+
+        for (int i = 0; i < parsedInput.length; i++) {
+            for (int j = 0; j < parsedInput[i].length; j++) {
+                var l = new Location(i, j);
+
+                if (visited.add(l)) {
+                    var r = new Region(parsedInput[i][j], new HashSet<>());
+                    check(l, r, parsedInput);
+                    visited.addAll(r.plots);
+                    regions.add(r);
+                }
+            }
+        }
+
+        return regions.stream()
+                .map(Region::price)
+                .reduce(0, Integer::sum);
+    }
+
+    private void check(Location location, Region region, char[][] parsedInput) {
+        var sizeX = parsedInput.length;
+        var sizeY = parsedInput[0].length;
+
+        region.plots.add(location);
+
+        location.neighbours().stream()
+                .filter(l -> inBounds(l, sizeX, sizeY))
+                .filter(l -> region.letter == parsedInput[l.x()][l.y()])
+                .forEach(ln -> check(ln, region, parsedInput));
     }
 
     @Override
     public Integer solve2(char[][] parsedInput) {
         return -1;
+    }
+
+    private record Region(char letter, Set<Location> plots) {
+        int price() {
+            return plots.size();
+        }
     }
 
 }
