93f41a63c2766b98bce1dff431eeb38497a3a03e
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-14 11:35:06 +0100

    2024 - Day 14 - prep

---
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day14.java
@@ -0,0 +1,33 @@
+package pl.vanta.adventofcode.year2024;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import pl.vanta.adventofcode.ParserSolver;
+import pl.vanta.adventofcode.Utils;
+
+import static java.lang.Integer.parseInt;
+
+public class Day14 implements ParserSolver<List<Day14.Machine>, Integer> {
+
+    @Override
+    public int getDayNumber() {
+        return 14;
+    }
+
+    @Override
+    public List<Machine> parse(String lines) {
+    }
+
+    @Override
+    public Integer solve(List<Machine> parsedInput) {
+        return -1;
+    }
+
+    @Override
+    public Integer solve2(List<Machine> parsedInput) {
+        return-1;
+    }
+
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day14Spec.groovy
@@ -0,0 +1,43 @@
+package pl.vanta.adventofcode.year2024
+
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+import spock.lang.Ignore
+
+class Day14Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        2024
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day14()
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [result: 480],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        31065
+    }
+
+    @Ignore
+    @Override
+    List<Map> getTestData2() {
+        [
+                [result: 875318608908],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        93866170395343
+    }
+}
