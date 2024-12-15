e221c41a0e813242cb1131c7a4fd9ab440709245
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-15 11:49:47 +0100

    2024 - Day 15 - prep

---
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day15.java
@@ -0,0 +1,57 @@
+package pl.vanta.adventofcode.year2024;
+
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import pl.vanta.adventofcode.Location;
+import pl.vanta.adventofcode.ParserSolver;
+
+public class Day15 implements ParserSolver<Day15.Input, Integer> {
+
+    @Override
+    public int getDayNumber() {
+        return 15;
+    }
+
+    @Override
+    public Day15.Input parse(String lines) {
+        var tokenizer = new StringTokenizer(lines, "\n");
+
+        var map = new ArrayList<char[]>();
+        var moves = new StringBuilder();
+
+        while (tokenizer.hasMoreTokens()) {
+            var line = tokenizer.nextToken();
+
+            if (line.startsWith("#")) {
+                map.add(line.toCharArray());
+            } else if (!line.isBlank()) {
+                moves.append(line);
+            }
+        }
+
+        return new Day15.Input(map.toArray(char[][]::new), moves.toString());
+    }
+
+    @Override
+    public Integer solve(Day15.Input parsedInput) {
+        return -1;
+    }
+
+    @Override
+    public Integer solve2(Day15.Input parsedInput) {
+        return -1;
+    }
+
+    public record Input(char[][] map, String moves) {
+
+    }
+
+    record Robot(Location location, char direction) {
+        Robot move(int sizeX, int sizeY) {
+            var newLocation = location.move(vx, vy).teleport(sizeX, sizeY);
+
+            return new Day14.Robot(newLocation, vx, vy);
+        }
+    }
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day15Spec.groovy
@@ -0,0 +1,47 @@
+
+package pl.vanta.adventofcode.year2024
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+
+class Day15Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        2024
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day15()
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [result: 10092],
+                [result: 2028, suffix: '-small'],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        233709840
+    }
+
+    @Override
+    List<Map> getTestData2() {
+        [
+                [],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        6620
+    }
+
+    @Override
+    boolean skipExamplePart2() {
+        true
+    }
+}
