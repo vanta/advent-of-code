9a6a38a143020e5078dacad26348a6dee22f2458
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-17 08:40:40 +0100

    2024 - Day 17 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day17.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day17.java
@@ -1,29 +1,42 @@
 package pl.vanta.adventofcode.year2024;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 import pl.vanta.adventofcode.ParserSolver;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.pow;
+import static java.util.stream.Collectors.joining;
 
 public class Day17 implements ParserSolver<Day17.Input, String> {
 
     @Override
     public int getDayNumber() {
-        return 16;
+        return 17;
     }
 
     @Override
     public Day17.Input parse(String lines) {
+        var tokenizer = new StringTokenizer(lines, "\n");
 
+        var a = parseInt(tokenizer.nextToken().substring(12));
+        var b = parseInt(tokenizer.nextToken().substring(12));
+        var c = parseInt(tokenizer.nextToken().substring(12));
+        var p = tokenizer.nextToken().substring(9).split(",");
 
-        return Arrays.stream(lines.split("\n"))
-                .map(String::toCharArray)
-                .toArray(char[][]::new);
+        return new Day17.Input(a, b, c, Arrays.stream(p).mapToInt(Integer::parseInt).toArray());
     }
 
     @Override
     public String solve(Day17.Input input) {
+        var computer = new Computer(input.a, input.b, input.c, input.program);
 
-        return "";
+        var out = computer.run();
+
+        return out.stream().map(String::valueOf).collect(joining(","));
     }
 
     @Override
@@ -31,6 +44,102 @@ public class Day17 implements ParserSolver<Day17.Input, String> {
         return "";
     }
 
-    record Input(String a, String b, String c, String program) {
+    public record Input(int a, int b, int c, int[] program) {
+    }
+
+    class Computer {
+        int a;
+        int b;
+        int c;
+        int[] program;
+
+        private int ip = 0;
+        private List<Integer> out = new ArrayList<>();
+
+        Computer(int a, int b, int c, int[] program) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.program = program;
+        }
+
+        private void adv(int param) {
+            a = a / (int) pow(2, combo(param));
+
+            ip += 2;
+        }
+
+        private void bxl(int param) {
+            b = b ^ param;
+
+            ip += 2;
+        }
+
+        private void bst(int param) {
+            b = combo(param) % 8;
+
+            ip += 2;
+        }
+
+        private void jnz(int param) {
+            if (a != 0) {
+                ip = param;
+            }
+        }
+
+        private void bxc(int param) {
+            b = b ^ c;
+
+            ip += 2;
+        }
+
+        private void out(int param) {
+            out.add(combo(param % 8));
+
+            ip += 2;
+        }
+
+        private void bdv(int param) {
+            b = a / (int) pow(2, combo(param));
+
+            ip += 2;
+        }
+
+        private void cdv(int param) {
+            c = a / (int) pow(2, combo(param));
+
+            ip += 2;
+        }
+
+        private int combo(int param) {
+            return switch (param) {
+                case 0, 1, 2, 3 -> param;
+                case 4 -> a;
+                case 5 -> b;
+                case 6 -> c;
+                default -> throw new IllegalArgumentException("Unknown param: " + param);
+            };
+        }
+
+        List<Integer> run() {
+            while (ip < program.length) {
+                var opcode = program[ip];
+                var param = program[ip + 1];
+
+                switch (opcode) {
+                    case 0 -> adv(param);
+                    case 1 -> bxl(param);
+                    case 2 -> bst(param);
+                    case 3 -> jnz(param);
+                    case 4 -> bxc(param);
+                    case 5 -> out(param);
+                    case 6 -> bdv(param);
+                    case 7 -> cdv(param);
+                    default -> throw new IllegalArgumentException("Unknown opcode: " + opcode);
+                }
+            }
+
+            return out;
+        }
     }
 }
