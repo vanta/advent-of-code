fc1213ac3ebd210090717b92a3349ffa91650ee5
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-19 14:41:40 +0100

    2024 - Day 18 - prep

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day18.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day18.java
@@ -1,14 +1,19 @@
 package pl.vanta.adventofcode.year2024;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import pl.vanta.adventofcode.Location;
 import pl.vanta.adventofcode.ParserSolver;
 
 import static java.lang.Integer.parseInt;
+import static java.util.stream.Collectors.*;
 
-public class Day18 implements ParserSolver<List<Location>, Integer> {
+public class Day18 implements ParserSolver<Set<Location>, Integer> {
 
     @Override
     public int getDayNumber() {
@@ -16,20 +21,39 @@ public class Day18 implements ParserSolver<List<Location>, Integer> {
     }
 
     @Override
-    public List<Location> parse(String lines) {
+    public Set<Location> parse(String lines) {
         return Arrays.stream(lines.split("\n"))
                 .map(s -> s.split(","))
                 .map(s -> new Location(parseInt(s[0]), parseInt(s[1])))
-                .toList();
+                .collect(toSet());
     }
 
     @Override
-    public Integer solve(List<Location> input) {
+    public Integer solve(Set<Location> input) {
+
+
+        var result = traverse(new Location(0, 0), new Location(6,6), input, new ArrayList<>(), new HashSet<>());
+
         return 0;
     }
 
+    private List<Location> traverse(Location current, Location end, Set<Location> input, List<Location> path, Set<Location> visited) {
+        if(!visited.add(current)) {
+            return path;
+        }
+
+        if(current.equals(end)) {
+            path.add(current);
+            return path;
+        }
+
+
+
+
+    }
+
     @Override
-    public Integer solve2(List<Location> parsedInput) {
+    public Integer solve2(Set<Location> input) {
         return 0;
     }
 
