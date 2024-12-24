eb1087bd8478e4f0427bc97ef48a137b1115b840
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-24 01:28:35 +0100

    2024 - Day 22 - part 2

--- src/main/groovy/pl/vanta/adventofcode/year2024/Day22.java
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day22.java
@@ -1,11 +1,13 @@
 package pl.vanta.adventofcode.year2024;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import pl.vanta.adventofcode.ParserSolver;
 
 import static java.lang.Math.floorDiv;
 import static java.util.Arrays.stream;
+import static java.util.Collections.indexOfSubList;
 import static java.util.stream.Stream.iterate;
 
 public class Day22 implements ParserSolver<List<Integer>, Long> {
@@ -29,23 +31,25 @@ public class Day22 implements ParserSolver<List<Integer>, Long> {
                 .reduce(0L, Long::sum);
     }
 
-    @Override
-    public Long solve2(List<Integer> input) {
-        return input.stream()
-                .map(i -> secret2(i, 2000))
-                .reduce(0L, Long::sum);
-    }
-
     private long secret(long initialSecret, int howMany) {
         return iterate(initialSecret, this::nextSecret)
                 .limit(howMany + 1)
                 .reduce(0L, (l1, l2) -> l2);
     }
 
-    private long secret2(long initialSecret, int howMany) {
+    @Override
+    public Long solve2(List<Integer> input) {
+        var windows = allWindows();
+
+        return input.stream()
+                .map(i -> secret2(i, 2000, windows))
+                .reduce(0L, Long::sum);
+    }
+
+    private long secret2(long initialSecret, int howMany, List<List<Integer>> windows) {
         var digits = iterate(initialSecret, this::nextSecret)
                 .limit(howMany + 1)
-                .map(l -> l % 10)
+                .map(l -> (int) (l % 10))
                 .toList();
 
         var diffs = iterate(1, i -> i + 1)
@@ -53,9 +57,36 @@ public class Day22 implements ParserSolver<List<Integer>, Long> {
                 .map(i -> digits.get(i) - digits.get(i - 1))
                 .toList();
 
+        return windows.stream()
+                .map(window -> indexOfSubList(diffs, window))
+                .filter(i -> i != -1)
+                .map(i -> digits.get(i + 4))
+                .max(Integer::compareTo)
+                .orElse(0);
+    }
 
+//    public static List<List<Long>> windows(List<Long> list, int windowSize) {
+//        if (list.size() < windowSize) {
+//            return List.of(); //
+//        }
+//
+//        return range(0, list.size() - windowSize + 1)
+//                .mapToObj(start -> list.subList(start, start + windowSize))
+//                .toList();
+//    }
 
-        return diffs.size();
+    public static List<List<Integer>> allWindows() {
+        List<List<Integer>> sequences = new ArrayList<>();
+        for (int i = -9; i <= 9; i++) {
+            for (int j = -9; j <= 9; j++) {
+                for (int k = -9; k <= 9; k++) {
+                    for (int l = -9; l <= 9; l++) {
+                        sequences.add(List.of(i, j, k, l));
+                    }
+                }
+            }
+        }
+        return sequences;
     }
 
     private long nextSecret(long secret) {
