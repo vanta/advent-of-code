daba948aec6adc0254c85e314e48885d30ac134e
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-24 12:16:11 +0100

    2024 - Day 23 - prep

---
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day23Spec.groovy
@@ -0,0 +1,42 @@
+
+
+package pl.vanta.adventofcode.year2024
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+
+class Day23Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        2024
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day23()
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [result: 7],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        19877757850
+    }
+
+    @Override
+    List<Map> getTestData2() {
+        [
+                [result: 23, suffix: '-part2'],
+        ]
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        2399
+    }
+}
