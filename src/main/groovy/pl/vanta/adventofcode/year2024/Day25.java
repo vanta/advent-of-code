b451e98a3248adf1681a424c57a7c11bcb8827a9
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2024-12-26 09:47:37 +0100

    2024 - Day 25 - prep

---
+++ src/main/groovy/pl/vanta/adventofcode/year2024/Day25.java
@@ -0,0 +1,27 @@
+package pl.vanta.adventofcode.year2024;
+
+import pl.vanta.adventofcode.ParserSolver;
+
+public class Day25 implements ParserSolver<Day25.Input, Integer> {
+
+    @Override
+    public int getDayNumber() {
+        return 24;
+    }
+
+    @Override
+    public Input parse(String lines) {
+
+    }
+
+    @Override
+    public Integer solve(Input input) {
+        return 1;
+    }
+
+    @Override
+    public Integer solve2(Input input) {
+        return 1;
+    }
+
+}
--- src/test/groovy/pl/vanta/adventofcode/year2024/Day25Spec.groovy
+++ src/test/groovy/pl/vanta/adventofcode/year2024/Day25Spec.groovy
@@ -3,7 +3,7 @@ package pl.vanta.adventofcode.year2024
 import pl.vanta.adventofcode.BaseSpec2024
 import pl.vanta.adventofcode.ParserSolverGeneric
 
-class Day24Spec extends BaseSpec2024 {
+class Day25Spec extends BaseSpec2024 {
     @Override
     int getYear() {
         2024
@@ -11,31 +11,30 @@ class Day24Spec extends BaseSpec2024 {
 
     @Override
     ParserSolverGeneric getParserSolver() {
-        new Day25()
+        new Day24()
     }
 
     @Override
     List<Map> getTestData1() {
         [
-                [result: 4],
-                [result: 2024, suffix: '-larger'],
+                [result: 3],
         ]
     }
 
     @Override
     Long getRealAnswer1() {
-        48063513640678
+        11
     }
 
     @Override
     List<Map> getTestData2() {
         [
-                [result: 'z00,z01,z02,z05', suffix: '-part2'],
+                [result: 'z00,z01,z02,z05'],
         ]
     }
 
     @Override
-    String getRealAnswer2() {
-        'aaaa'
+    Long getRealAnswer2() {
+        3
     }
 }
