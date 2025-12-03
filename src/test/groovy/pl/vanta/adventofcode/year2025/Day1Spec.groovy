f995ecc0ff31931b80ee0478b65f8df0da6f3c03
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-03 18:11:05 +0100

    Day1 scaffold

---
+++ src/test/groovy/pl/vanta/adventofcode/year2025/Day1Spec.groovy
@@ -0,0 +1,31 @@
+package pl.vanta.adventofcode.year2025
+
+import pl.vanta.adventofcode.BaseSpec
+import pl.vanta.adventofcode.ParserSolver
+
+class Day1Spec extends BaseSpec {
+    @Override
+    ParserSolver getParserSolver() {
+        new Day1()
+    }
+
+    @Override
+    Integer getExampleAnswer1() {
+        11
+    }
+
+    @Override
+    Integer getRealAnswer1() {
+        1223326
+    }
+
+    @Override
+    Integer getExampleAnswer2() {
+        31
+    }
+
+    @Override
+    Integer getRealAnswer2() {
+        21070419
+    }
+}
