0d310e9ba4266602fa6b52698259d5737cce0005
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-05 16:44:56 +0100

    Day4 - part 1

---
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day4.java
@@ -0,0 +1,55 @@
+package pl.vanta.adventofcode.year2025;
+
+import java.util.stream.Stream;
+
+public class Day4 extends BaseDay<char[][], Integer> {
+    @Override
+    public int getDayNumber() {
+        return 4;
+    }
+
+    @Override
+    public char[][] parse(String lines) {
+        return Stream.of(lines.trim().split("\n"))
+                .map(String::toCharArray)
+                .toArray(char[][]::new);
+    }
+
+    @Override
+    public Integer solve(char[][] parsedInput) {
+        var result = 0;
+
+        for (int i = 0; i < parsedInput.length; i++) {
+            for (int j = 0; j < parsedInput[i].length; j++) {
+                if (parsedInput[i][j] == '@') {
+                    if (countNeighbors(parsedInput, i, j) < 4) {
+                        result++;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int countNeighbors(char[][] parsedInput, int i, int j) {
+        int result = 0;
+
+        if (i > 0 && parsedInput[i - 1][j] == '@') result++; // up
+        if (i < parsedInput.length - 1 && parsedInput[i + 1][j] == '@') result++; // down
+        if (j > 0 && parsedInput[i][j - 1] == '@') result++; // left
+        if (j < parsedInput[i].length - 1 && parsedInput[i][j + 1] == '@') result++; // right
+        if (i > 0 && j > 0 && parsedInput[i - 1][j - 1] == '@') result++; // up-left
+        if (i > 0 && j < parsedInput[i].length - 1 && parsedInput[i - 1][j + 1] == '@') result++; // up-right
+        if (i < parsedInput.length - 1 && j > 0 && parsedInput[i + 1][j - 1] == '@') result++; // down-left
+        if (i < parsedInput.length - 1 && j < parsedInput[i].length - 1 && parsedInput[i + 1][j + 1] == '@') result++; // down-right
+
+        return result;
+    }
+
+    @Override
+    public Integer solve2(char[][] parsedInput) {
+        return 0;
+    }
+
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2025/Day4Spec.groovy
@@ -0,0 +1,40 @@
+package pl.vanta.adventofcode.year2025
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+
+class Day4Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        return 2025
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day4()
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        1416
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        -1
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [result: 13]
+        ]
+    }
+
+    @Override
+    List<Map> getTestData2() {
+        [
+                [result: -1],
+        ]
+    }
+}
