a08de4a00c5af0ad6902ac9b47083b04ea3da0f5
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-06 12:26:38 +0100

    Day 6 - input

--- src/main/groovy/pl/vanta/adventofcode/year2025/Day6.java
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day6.java
@@ -1,17 +1,7 @@
 package pl.vanta.adventofcode.year2025;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-
-import org.apache.commons.lang3.LongRange;
-import org.apache.commons.lang3.Range;
-
-import static java.lang.Long.parseLong;
-import static java.lang.Math.max;
-import static java.lang.Math.min;
-import static java.util.Comparator.comparingLong;
-import static java.util.stream.Collectors.toSet;
 
 public class Day6 extends BaseDay<Day6.Input, Long> {
     @Override
@@ -25,67 +15,29 @@ public class Day6 extends BaseDay<Day6.Input, Long> {
                 .map(String::trim)
                 .toList();
 
-        var blankIndex = lines.indexOf("");
-
-        var ranges = lines.subList(0, blankIndex).stream()
-                .map(s -> s.split("-"))
-                .map(a -> LongRange.of(parseLong(a[0]), parseLong(a[1])))
-                .sorted(comparingLong(Range::getMinimum))
+        var numbers = lines.subList(0, lines.size() - 1).stream()
+                .map(line -> Arrays.stream(line.split("\\s+"))
+                        .map(Integer::parseInt)
+                        .toList())
                 .toList();
 
-        var ids = lines.subList(blankIndex + 1, lines.size())
-                .stream()
-                .map(Long::parseLong)
-                .collect(toSet());
+        var operations = Arrays.stream(lines.getLast().split("\\s+"))
+                .map(s -> s.charAt(0))
+                .toList();
 
-        return new Input(ranges, ids);
+        return new Input(numbers, operations);
     }
 
     @Override
     public Long solve(Day6.Input parsedInput) {
-        return parsedInput.ids.stream()
-                .filter(id -> parsedInput.ranges.stream().anyMatch(r -> r.contains(id)))
-                .count();
+        return 0L;
     }
 
     @Override
     public Long solve2(Day6.Input parsedInput) {
-        var currentRanges = new ArrayList<LongRange>();
-        var newRanges = new ArrayList<>(parsedInput.ranges);
-        boolean merged;
-
-        do {
-            currentRanges = new ArrayList<>(newRanges);
-            newRanges.clear();
-            for (int i = 0; i < currentRanges.size(); i++) {
-                merged = false;
-                for (int j = i + 1; j < currentRanges.size(); j++) {
-                    if (overlap(currentRanges.get(i), currentRanges.get(j))) {
-                        newRanges.add(merge(currentRanges.get(i), currentRanges.get(j)));
-                        merged = true;
-                        i += 1;
-                        break;
-                    }
-                }
-                if (!merged) {
-                    newRanges.add(currentRanges.get(i));
-                }
-            }
-        } while (currentRanges.size() != newRanges.size());
-
-        return currentRanges.stream()
-                .map(r -> r.getMaximum() - r.getMinimum() + 1)
-                .reduce(0L, Long::sum);
+        return 0L;
     }
 
-    private boolean overlap(LongRange r1, LongRange r2) {
-        return r1.getMaximum() >= r2.getMinimum() && r2.getMaximum() >= r1.getMinimum();
-    }
-
-    private LongRange merge(LongRange r1, LongRange r2) {
-        return LongRange.of(min(r1.getMinimum(), r2.getMinimum()), max(r1.getMaximum(), r2.getMaximum()));
-    }
-
-    public record Input(List<LongRange> ranges, Set<Long> ids) {
+    public record Input(List<List<Integer>> numbers, List<Character> operations) {
     }
 }
