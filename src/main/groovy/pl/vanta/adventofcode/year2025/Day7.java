06ee9cb8c6feeec40dceb831c41cbc3629ac4f15
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-07 18:13:57 +0100

    Day 7 - part 2 - wip

--- src/main/groovy/pl/vanta/adventofcode/year2025/Day7.java
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day7.java
@@ -4,11 +4,8 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import pl.vanta.adventofcode.Utils;
-
 import static com.google.common.collect.Sets.union;
 import static java.util.Arrays.stream;
-import static java.util.stream.Collectors.joining;
 import static pl.vanta.adventofcode.Utils.indexesOf;
 
 public class Day7 extends BaseDay<List<String>, Integer> {
@@ -46,27 +43,24 @@ public class Day7 extends BaseDay<List<String>, Integer> {
 
     @Override
     public Integer solve2(List<String> parsedInput) {
-        var beams = new HashSet<Integer>();
         var start = parsedInput.getFirst().indexOf('S');
-
-        return move(parsedInput.subList(1, parsedInput.size()), List.of(start)).size();
+        return move(parsedInput.subList(1, parsedInput.size()), String.valueOf(start), start).size();
     }
 
-    private static Set<String> move(List<String> list, List<Integer> path) {
+    private static Set<String> move(List<String> list, String path, int index) {
         if (list.isEmpty()) {
-            return Set.of(path.stream().map(String::valueOf).collect(joining()));
+            return Set.of(path);
         }
 
         var sublist = list.subList(1, list.size());
-        var index = path.getLast();
 
         if (indexesOf(list.getFirst(), '^').contains(index)) {
             return union(
-                    move(sublist, Utils.add(path, index - 1)),
-                    move(sublist, Utils.add(path, index + 1))
+                    move(sublist, path + (index - 1), index - 1),
+                    move(sublist, path + (index + 1), index + 1)
             );
         } else {
-            return move(sublist, Utils.add(path, index));
+            return move(sublist, path + index, index);
         }
     }
 }
