c0268af186f2bdcbc049dbe3a304d90a021c621b
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-08 07:08:26 +0100

    Day 8 - part 1

--- src/main/groovy/pl/vanta/adventofcode/year2025/Day8.java
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day8.java
@@ -1,10 +1,12 @@
 package pl.vanta.adventofcode.year2025;
 
+import java.util.Comparator;
 import java.util.List;
 
 import org.apache.commons.lang3.tuple.Triple;
 
 import static java.lang.Integer.parseInt;
+import static java.lang.Math.*;
 import static java.util.Arrays.stream;
 
 public class Day8 extends BaseDay<List<Triple<Integer, Integer, Integer>>, Long> {
@@ -23,7 +25,9 @@ public class Day8 extends BaseDay<List<Triple<Integer, Integer, Integer>>, Long>
 
     @Override
     public Long solve(List<Triple<Integer, Integer, Integer>> parsedInput) {
-
+                
+        
+        
         return 0L;
     }
 
@@ -34,5 +38,9 @@ public class Day8 extends BaseDay<List<Triple<Integer, Integer, Integer>>, Long>
         return 0L;
     }
 
-
+    private static long distance(Triple<Integer, Integer, Integer> p1, Triple<Integer, Integer, Integer> p2) {
+        return powExact(p1.getLeft() - p2.getLeft(), 2)
+                + powExact(p1.getMiddle() - p2.getMiddle(), 2)
+                + powExact(p1.getRight() - p2.getRight(), 2);
+    }
 }
