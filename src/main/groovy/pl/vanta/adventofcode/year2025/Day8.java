81f692d910f6b65d357c4425e4b91d80729f07e2
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-08 06:55:52 +0100

    Day 8 - scaffold

---
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day8.java
@@ -0,0 +1,78 @@
+package pl.vanta.adventofcode.year2025;
+
+import java.util.List;
+import java.util.Set;
+
+import static com.google.common.collect.Sets.union;
+import static java.util.Arrays.stream;
+import static pl.vanta.adventofcode.Utils.indexesOf;
+
+public class Day8 extends BaseDay<List<String>, Long> {
+    @Override
+    public int getDayNumber() {
+        return 8;
+    }
+
+    @Override
+    public List<String> parse(String input) {
+        return stream(input.split("\n"))
+                .filter(line -> line.contains("^") || line.contains("S"))
+                .toList();
+    }
+
+    @Override
+    public Long solve(List<String> parsedInput) {
+        var splits = 0L;
+
+        var beams = new boolean[parsedInput.getFirst().length()];
+        beams[parsedInput.getFirst().indexOf('S')] = true;
+
+        for (String line : parsedInput.subList(1, parsedInput.size())) {
+            for (int i = 0; i < line.length(); i++) {
+                if (beams[i] && line.charAt(i) == '^') {
+                    splits++;
+                    beams[i] = false;
+                    beams[i - 1] = true;
+                    beams[i + 1] = true;
+                }
+            }
+        }
+
+        return splits;
+    }
+
+    @Override
+    public Long solve2(List<String> parsedInput) {
+        var beams = new long[parsedInput.getFirst().length()];
+        beams[parsedInput.getFirst().indexOf('S')] = 1L;
+
+        for (String line : parsedInput.subList(1, parsedInput.size())) {
+            for (int i = 0; i < line.length(); i++) {
+                if (beams[i] > 0 && line.charAt(i) == '^') {
+                    beams[i - 1] += beams[i];
+                    beams[i + 1] += beams[i];
+                    beams[i] = 0;
+                }
+            }
+        }
+
+        return stream(beams).sum();
+    }
+
+    private static Set<String> move(List<String> list, String path, int index) {
+        if (list.isEmpty()) {
+            return Set.of(path);
+        }
+
+        var sublist = list.subList(1, list.size());
+
+        if (indexesOf(list.getFirst(), '^').contains(index)) {
+            return union(
+                    move(sublist, path + (index - 1), index - 1),
+                    move(sublist, path + (index + 1), index + 1)
+            );
+        } else {
+            return move(sublist, path + index, index);
+        }
+    }
+}
---
+++ src/test/groovy/pl/vanta/adventofcode/year2025/Day8Spec.groovy
@@ -0,0 +1,41 @@
+package pl.vanta.adventofcode.year2025
+
+import pl.vanta.adventofcode.BaseSpec2024
+import pl.vanta.adventofcode.ParserSolver
+
+class Day8Spec extends BaseSpec2024 {
+    @Override
+    int getYear() {
+        return 2025
+    }
+
+    @Override
+    ParserSolver getParserSolver() {
+        new Day8()
+    }
+
+    @Override
+    Long getRealAnswer1() {
+        -1
+    }
+
+    @Override
+    Long getRealAnswer2() {
+        -1
+    }
+
+    @Override
+    List<Map> getTestData1() {
+        [
+                [pairs: 10],
+                [result: 40]
+        ]
+    }
+
+    @Override
+    List<Map> getTestData2() {
+        [
+                [result: -1],
+        ]
+    }
+}
