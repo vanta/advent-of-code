cbab6e6ac6fa3a2478894b21995ffca8cfc12fcb
Krzysztof Wolny <k.wolny@smartrecruiters.com>
2025-12-10 00:47:20 +0100

    Day 9 - part 2 wip

--- src/main/groovy/pl/vanta/adventofcode/year2025/Day9.java
+++ src/main/groovy/pl/vanta/adventofcode/year2025/Day9.java
@@ -63,6 +63,11 @@ public class Day9 extends BaseDay<List<Pair<Integer, Integer>>, Long> {
     }
 
     private static boolean contains(List<Pair<Integer, Integer>> poly, int px, int py) {
+        //vertex
+        if (poly.contains(Pair.of(px, py))) {
+            return true;
+        }
+
         boolean inside = false;
         int n = poly.size();
 
@@ -70,10 +75,6 @@ public class Day9 extends BaseDay<List<Pair<Integer, Integer>>, Long> {
             var a = poly.get(i);
             var b = poly.get((i + 1) % n);
 
-            if(px == a.getLeft() && py == a.getRight() || px == b.getLeft() && py == b.getRight()) {
-                return true;
-            }
-            
             // Only consider vertical edges
             if (a.getLeft().equals(b.getLeft())) {
                 int x = a.getLeft();
